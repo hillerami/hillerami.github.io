57ab4e12081f469af87db632878c4c69dfa7aba3
kevin <k6yk07@gmail.com>
2023-04-24 21:26:35 -0400

    update to use svg

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
     // Your code here
     console.log("DOM is ready!");
     // You can now safely access and manipulate the DOM elements
-    const currentYear = new Date().getFullYear();
+
+    var today = new Date();
+    const currentYear = today.getFullYear();
     document.getElementById("currentYear").textContent = currentYear;
+
+
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+     
+    console.log(dateTime)
 });
