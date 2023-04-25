be6ce3836e6c4a11e570f47a249192aa53766a70
kevin <k6yk07@gmail.com>
2023-04-24 22:10:56 -0400

    add date

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,29 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("DOM is ready!");
     // You can now safely access and manipulate the DOM elements
 
-    var today = new Date();
-    const currentYear = today.getFullYear();
-    document.getElementById("currentYear").textContent = currentYear;
-
-
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var dateTime = date+' '+time;
-     
-    console.log(dateTime)
+    setDate();
 });
+
+
+const setDate = () => {
+
+    const now = new Date();
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const currentMonth = now.toLocaleString('default', { month: 'long' });
+    const day = weekday[now.getDay()];
+    const currentHour = now.getHours();
+    const hourIn12HourFormat = currentHour % 12 || 12;
+    const amOrPm = currentHour >= 12 ? 'PM' : 'AM';
+    const minutes = ('0' + now.getMinutes()).slice(-2);
+
+    const verboseDate = `Today is ${day}, ${currentMonth} ${now.getUTCDate()}`;
+    const verboseTime = `The current time is ${hourIn12HourFormat}:${minutes} ${amOrPm}`;
+
+    document.getElementById("current-date").textContent = verboseDate;
+    document.getElementById("current-time").textContent = verboseTime;
+
+    // also set footer year
+    document.getElementById("currentYear").textContent = now.getFullYear();
+}
+
