448598aebcbb77690494ca02f27173b6436d3a2b
kevin <k6yk07@gmail.com>
2023-04-25 23:48:49 -0400

    bold time

--- scripts/index.js
+++ scripts/index.js
@@ -13,19 +13,19 @@ const setDate = () => {
     const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
     const currentMonth = now.toLocaleString('default', { month: 'long' });
-    const day = weekday[now.getDay()];
+    const day = weekday[now.getDay()].toString();
     const currentHour = now.getHours();
     const hourIn12HourFormat = currentHour % 12 || 12;
     const amOrPm = currentHour >= 12 ? 'PM' : 'AM';
     const minutes = ('0' + now.getMinutes()).slice(-2);
 
-    const verboseDate = `Today is ${day}, ${currentMonth} ${now.getUTCDate()}`;
-    const verboseTime = `Current time is ${hourIn12HourFormat}:${minutes} ${amOrPm}`;
+    const verboseDate = `Today is <strong>${day}, ${currentMonth} ${now.getUTCDate()}</strong>`;
+    const verboseTime = `Current time is <strong>${hourIn12HourFormat}:${minutes} ${amOrPm}</strong>`;
 
-    document.getElementById("current-date").textContent = verboseDate;
-    document.getElementById("current-time").textContent = verboseTime;
+    document.getElementById("current-date").innerHTML = verboseDate;
+    document.getElementById("current-time").innerHTML = verboseTime;
 
     // also set footer year
-    document.getElementById("currentYear").textContent = now.getFullYear();
+    document.getElementById("currentYear").innerHTML = now.getFullYear();
 }
 
