9eea30205393968995f8ae38e119d670b4f62e00
kevin <k6yk08@gmail.com>
2023-05-08 20:43:17 -0400

    xmlhttp test

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,17 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
-    displayArticle();
-});
-
-
-
-const displayArticle = async () => {
-    console.log(window.location);
-    fetch('../files/writings/articles.txt')
-    .then(response => response.text())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
+window.addEventListener('DOMContentLoaded', () => {
+    const fileURL = '../files/writings/articles.txt'; // Change 'textfile.txt' to the name of your text file
+    fetchFileContent(fileURL)
+      .then((content) => {
+        displayTextFileContent(content);
+      })
+      .catch((error) => {
         console.error('Error reading file:', error);
+      });
+  });
+  
+  function fetchFileContent(fileURL) {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', fileURL, true);
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            resolve(xhr.responseText);
+          } else {
+            reject(xhr.statusText);
+          }
+        }
+      };
+      xhr.send();
     });
-}
+  }
+  
+  function displayTextFileContent(content) {
+    const lines = content.split('\n');
+    const container = document.getElementById('content');
+  
+    lines.forEach((line) => {
+      if (line.trim().length > 0) {
+        const paragraph = document.createElement('p');
+        paragraph.textContent = line.trim();
+        container.appendChild(paragraph);
+      }
+    });
+  }
+  
